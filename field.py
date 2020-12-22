25d75e2e272a782ba0f6c1381d9ad59030cab02f
vishesh-khemani <vishesh.khemani@gmail.com>
2020-12-22 14:26:03 -0800

    2^n finite fields for n in [2, 7].

---
+++ field.py
@@ -0,0 +1,82 @@
+class BinaryFiniteField:
+    """2^n extension finite field for n in [2, 7]."""
+
+    def __init__(self, n):
+        """2^n field. """
+
+        assert n in range(2, 9), "n must be in [2, 7]"
+        self.n_ = n
+        self.order_ = 1 << n
+
+        # Irreducible polynomial for mod multiplication.
+        if n == 2:
+            self.divisor_ = 7  # 1 + x + x^2
+        elif n == 3:
+            self.divisor_ = 11  # 1 + x + x^3
+        elif n == 4:
+            self.divisor_ = 19  # 1 + x + x^4
+        elif n == 5:
+            self.divisor_ = 37  # 1 + x^2 + x^5
+        elif n == 6:
+            self.divisor_ = 67  # 1 + x + x^6
+        elif n == 7:
+            self.divisor_ = 131  # 1 + x + x^7
+        else:
+            raise ValueError("n must be in [2, 7]")
+
+    def order(self):
+        return self.order_
+
+    def validated(self, a):
+        assert a in range(self.order_)
+        return a
+
+    def add(self, a, b):
+        return self.validated(self.validated(a) ^ self.validated(b))
+
+    def negate(self, a):
+        return self.validated(a)
+
+    def subtract(self, a, b):
+        return self.add(a, self.negate(b))
+
+    def multiply(self, a, b):
+        self.validated(a)
+        result = 0
+        bin_b = bin(self.validated(b))[2:]  # remove the '0b' prefix
+        shift = len(bin_b) - 1
+        for d in bin_b:
+            if d == '1':
+                result = result ^ (a << shift)
+            shift = shift - 1
+        while result >= self.order_:
+            shift = len(bin(result)) - len(bin(self.divisor_))
+            result = result ^ (self.divisor_ << shift)
+        return self.validated(result)
+
+    def invert(self, a):
+        if self.validated(a) == 0:
+            raise Exception("0 has no inverse")
+        for b in range(self.order_):
+            if self.multiply(a, b) == 1:
+                return b
+        raise Exception("No inverse found for {0}".format(a))
+
+    def divide(self, a, b):
+        return self.multiply(a, self.invert(b))
+
+    def matrix(self, a):
+        """Returns the n by n binary matrix representation of 'a'."""
+
+        self.validated(a)
+        result = [[0] * self.n_ for i in range(self.n_)]
+        basis_vec = 1
+        for c in range(self.n_):
+            p = self.multiply(a, basis_vec)
+            bin_p = bin(p)[2:]
+            while len(bin_p) < self.n_:
+                bin_p = '0' + bin_p
+            basis_vec = basis_vec << 1
+            for r in range(self.n_):
+                result[r][c] = int(bin_p[-r-1])
+        return result
---
+++ field_test.py
@@ -0,0 +1,67 @@
+import field
+import unittest
+
+
+class TestBinaryFiniteField(unittest.TestCase):
+    @classmethod
+    def setUpClass(cls):
+        cls.field_ = []
+        for n in range(2, 8):
+            cls.field_.append(field.BinaryFiniteField(n))
+
+    def test_add(self):
+        for field in TestBinaryFiniteField.field_:
+            negatives = {}
+            for a in range(field.order()):
+                for b in range(a, field.order()):
+                    result = field.add(a, b)
+                    self.assertTrue(result in range(0, field.order()))
+                    self.assertEqual(result, field.add(b, a))
+                    if result == 0:
+                        negatives[a] = b
+                        negatives[b] = a
+            self.assertEqual(field.order(), len(negatives))
+
+    def test_subtract(self):
+        for field in self.field_:
+            for a in range(field.order()):
+                for b in range(field.order()):
+                    result = field.subtract(a, b)
+                    self.assertTrue(result in range(0, field.order()))
+                    if a == b:
+                        self.assertEqual(0, result)
+                    else:
+                        self.assertTrue(result > 0)
+
+    def test_multiply(self):
+        for field in self.field_:
+            reciprocals = {}
+            for a in range(field.order()):
+                for b in range(field.order()):
+                    result = field.multiply(a, b)
+                    self.assertTrue(result in range(0, field.order()))
+                    self.assertEqual(result, field.multiply(b, a))
+                    if result == 0:
+                        self.assertTrue(a == 0 or b == 0)
+                    if result == 1:
+                        self.assertFalse(a == 0 or b == 0)
+                        reciprocals[a] = b
+                        reciprocals[b] = a
+            self.assertEqual(field.order() - 1, len(reciprocals))
+
+    def test_divide(self):
+        for field in self.field_:
+            for a in range(field.order()):
+                for b in range(field.order()):
+                    if b == 0:
+                        continue
+                    result = field.divide(a, b)
+                    self.assertTrue(result in range(0, field.order()))
+                    if a == b:
+                        self.assertEqual(1, result)
+                    else:
+                        self.assertNotEqual(1, result)
+
+
+if __name__ == '__main__':
+    unittest.main()
